e4b45ed45595440a8e961961978f2aabdd9073ab
Marius Agerup <Marius.schei@outlook.com>
2023-04-07 23:37:48 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import "./sass/style.scss";
 import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
