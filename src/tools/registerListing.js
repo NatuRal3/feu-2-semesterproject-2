7fa890eedda9e7b6264d7fca7748bcb8cda912a7
Marius Agerup <Marius.schei@outlook.com>
2023-04-21 23:52:28 +0200

    I love my job, i love my job

--- src/tools/registerListing.js
+++ src/tools/registerListing.js
@@ -2,6 +2,12 @@ import React from "react";
 
 function registerListing() {
   console.log("Create listing Button pushed");
+
+  const listingTitleInput = document.getElementById("formNewListingTitle");
+
+  const listingDeadlineInput = document.getElementById("formNewListingTitle");
+
+  const listingImageInput = document.getElementById("formNewListingTitle");
 }
 
 export default registerListing;
