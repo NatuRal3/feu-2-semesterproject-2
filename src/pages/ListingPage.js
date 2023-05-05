5d0d4727a2f1afa93ce27b8ea8f9d64c5b6229ac
Marius Agerup <Marius.schei@outlook.com>
2023-05-05 23:34:46 +0200

    Styling and starting on dates

--- src/pages/ListingPage.js
+++ src/pages/ListingPage.js
@@ -9,20 +9,41 @@ function ListingPage() {
   useEffect(() => {
     async function fetchData() {
       const data = await getListing(listingId);
+      console.log(data);
       setListing(data);
     }
     fetchData();
   }, [listingId]);
+
+  useEffect(() => {
+    if (listing) {
+      const endDate = new Date(listing.endsAt);
+      console.log(endDate);
+    }
+  });
 
   if (!listing) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>{listing.title}</h1>
-      <img src={listing.media} alt={listing.title} />
-      <p>{listing.description}</p>
+    <div className="container-listing flex center">
+      <div className="card-listing flex ">
+        <div className="card-info-media">
+          <img src={listing.media} alt={listing.title} />
+        </div>
+        <div className="card-info-txt flex column">
+          <div>
+            <h1>{listing.title}</h1>
+            <p>{listing.description}</p>
+            <p>Ends: {listing.endsAt}</p>
+          </div>
+          <div className="flex row">
+            <p>Bids: {listing._count.bids}</p>
+            <p>Highest bid:</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
